72c3373805daaa81a92289d1bf487b97710ee5ff
newaij <poontzeyang@gmail.com>
2020-05-16 00:21:49 +0800

    documentation for polynomial_fact.py

--- polynomial_fact.py
+++ polynomial_fact.py
@@ -1,6 +1,24 @@
 from polynomial import *
 from random import randint
 
+"""
+FUNCTIONS IMPLEMENTED: FACTORISE_POLYNOMIAL_INT_FINITE
+	- factorises integer polynomial with coefficients in finite field fully 
+	  into irreducible factors (irreducible in Fn[x])
+
+	- INPUT 	: polynomial with integer coefficients in finite field
+	- OUTPUT 	: list of factors
+
+- HELPER FUNCTIONS
+	- equal_degree_factorisation(p, d) 	- factorises squarefree polynomial (p) with factors only of equal degree (d)
+	- equal_degree_splitting(p, d)		- returns one factor of squarefree polynomial (p) with factors only of equal degree (d)
+
+- GENERAL PURPOSE FUNCTIONS
+	- rand_polynomial(max_degree)		- random polynomial in finite field FN of maximum degree (max_degree)
+	- filter_degree(p, min_degree=1)	- returns true if degree of p >= min_degree
+
+"""
+
 
 def rand_polynomial(max_degree):
     p = polynomial()
@@ -13,7 +31,12 @@ def filter_degree(p, min_degree=1):
     return p.degree >= min_degree
 
 
+class UnluckyStartError(Exception):
+    pass
+
+
 """
+FUNCTION: EQUAL_DEGREE_SPLITTING
 Inputs:
 - p : squarefree polynomial of degree n
 - d : target degree of factors (integer divisor of n)
@@ -21,10 +44,6 @@ Inputs:
 Output:
 - a factor of p of degree d or failure exception (happens probabilistically)
 """
-
-
-class UnluckyStartError(Exception):
-    pass
 
 
 def equal_degree_splitting(p, d):
@@ -35,7 +54,7 @@ def equal_degree_splitting(p, d):
     g1 = gcd_polynomial(a, p)
     p_one = polynomial(size=g1.size)
     p_one.set_coef(0, 1)
-    if g1.degree > 0:  # not np.all(np.equal(p_one.coef, g1.coef))):
+    if g1.degree > 0:
         return g1
 
     b = exp_polynomial_rem(a, int((N ** d - 1) / 2), p)
@@ -44,9 +63,7 @@ def equal_degree_splitting(p, d):
     g2 = gcd_polynomial(b, p)
     p_one = polynomial(size=g2.size)
     p_one.set_coef(0, 1)
-    if g2.degree > 0 and not np.all(
-        np.equal(p.coef, g2.coef)
-    ):  # not np.all(np.equal(p_one.coef, g2.coef)) and not np.all(np.equal(p.coef, g2.coef))):
+    if g2.degree > 0 and not np.all(np.equal(p.coef, g2.coef)):
         return g2
     else:
         raise UnluckyStartError
@@ -71,27 +88,22 @@ Output: list of polynomial factors of p
 """
 
 
-def fact_polynomial(p):
+def factorise_polynomial_int_finite(p):
     h = polynomial()
     h.set_coef(1, 1)
 
-    # keeps track of remainder of polynomial
-    # lc_inv = polynomial()
-    # lc_inv.set_coef(0,mul_inv(p.get_coef(p.degree)))
-    v = p  # mul_polynomial(p,lc_inv)
+    v = p
 
     i = 0
 
     # set of factors found
     U = []
-    # const = polynomial()
-    # const.set_coef(0,p.get_coef(p.degree))
 
     v_one = polynomial()
     v_one.set_coef(0, 1)
     v_zero = polynomial()
 
-    while v.degree > 0:  # not equal_polynomial(v_one,v)):
+    while v.degree > 0:
         i += 1
 
         """
@@ -116,46 +128,35 @@ def fact_polynomial(p):
                 while equal_polynomial(mod_polynomial(v, fac)[1], v_zero):
                     U.append(fac)
                     v = mod_polynomial(v, fac)[0]
-    # v = mod_polynomial(v,g)[0]
-    # lc_inv = mul_polynomial(lc_inv,g)
-
-    # lc_inv = mul_polynomial(lc_inv,v)
-    # U.append(lc_inv)
 
     return U
 
 
-p0 = polynomial()
-p3 = polynomial()
-p3.set_coef(0, 2)
-p3.set_coef(1, 3)
-p3.set_coef(2, 1)
-p4 = polynomial()
-p4.set_coef(0, 1)
-p4.set_coef(1, 3)
-p4.set_coef(2, 3)
-p4.set_coef(3, 1)
-p_prob = polynomial()
-p_prob.set_coef(0, 8)
-p_prob.set_coef(1, 12)
-p_prob.set_coef(2, 19)
-p_prob.set_coef(3, 20)
-p_prob.set_coef(4, 6)
-p_prob.set_coef(5, 18)
-# print("p3:", p3)
-p_rand = rand_polynomial(6)
-print("random_polynomial: ", p_rand)
-# print("p4:", p4)
+if __name__ == "__main__":
 
-# print("equal_degree_factorisation",equal_degree_factorisation(p4,1))
-print("factorisation: ")
-facs = fact_polynomial(p_rand)
+    p0 = polynomial()
+    p3 = polynomial()
+    p3.set_coef(0, 2)
+    p3.set_coef(1, 3)
+    p3.set_coef(2, 1)
+    p4 = polynomial()
+    p4.set_coef(0, 1)
+    p4.set_coef(1, 3)
+    p4.set_coef(2, 3)
+    p4.set_coef(3, 1)
+    p_prob = polynomial()
+    p_prob.set_coef(0, 8)
+    p_prob.set_coef(1, 12)
+    p_prob.set_coef(2, 19)
+    p_prob.set_coef(3, 20)
+    p_prob.set_coef(4, 6)
+    p_prob.set_coef(5, 18)
+    # print("p3:", p3)
+    p_rand = rand_polynomial(6)
+    print("random_polynomial: ", p_rand)
+    print("factorisation: ")
+    facs = factorise_polynomial_int_finite(p_rand)
 
-for f in facs:
-    print(f)
-print("verify: ", mul_polynomial(*facs))
-
-# print("split: ", gcd_polynomial(p3, p4))
-
-# p5 = rand_polynomial(4)
-# print("p_rand: ", filter_degree(p0))
+    for f in facs:
+        print(f)
+    print("verify: ", mul_polynomial(*facs))
