e8d12f91c4f77bd9f76302d401f1a6cceff74a88
newaij <poontzeyang@gmail.com>
2020-05-20 13:43:26 +0800

    Reorg

--- logic/logic.py
+++ logic/logic.py
@@ -1,43 +1,60 @@
 class logic:
-    def __init__(self, b = None):
+    def __init__(self, b=None):
         self.bool = b
+
     def __xor__(self, other):
         result = logic()
         result.bool = self.bool and other.bool
         return result
+
     def __or__(self, other):
         result = logic()
         result.bool = self.bool or other.bool
         return result
+
     def __invert__(self):
         result = logic()
         result.bool = not self.bool
         return result
+
     def __ge__(self, other):
         result = logic()
-        result.bool =(not self.bool) or other.bool
+        result.bool = (not self.bool) or other.bool
         return result
+
     def __str__(self):
         return str(self.bool)
-    
-def logicOutput(P,Q,R):
-    return (P>=(~Q|R))^(R|Q)
 
-num = 3 #number of variables
+
+def logicOutput(P, Q, R):
+    return (P >= (~Q | R)) ^ (R | Q)
+
+
+num = 3  # number of variables
 varspace = 3
 outspace = 8
-print("P".rjust(varspace," "), "Q".rjust(varspace," "), "R".rjust(varspace," "), "output".rjust(outspace," "),end ="\n\n")
-for i in range(1<<num):#2^number of variables
+print(
+    "P".rjust(varspace, " "),
+    "Q".rjust(varspace, " "),
+    "R".rjust(varspace, " "),
+    "output".rjust(outspace, " "),
+    end="\n\n",
+)
+for i in range(1 << num):  # 2^number of variables
     varVal = []
     for x in range(num):
         result = logic()
-        result.bool = bool(i&(1<<x))
+        result.bool = bool(i & (1 << x))
         varVal.append(result)
-    
-    [print("T".rjust(varspace," "), end = " ") if x.bool \
-     else print("F".rjust(varspace," "),end=" ") for x in varVal]
+
+    [
+        print("T".rjust(varspace, " "), end=" ")
+        if x.bool
+        else print("F".rjust(varspace, " "), end=" ")
+        for x in varVal
+    ]
     if logicOutput(*varVal).bool:
         out = "T"
-    else: out = "F"
-    print(out.rjust(outspace," "))
-    
+    else:
+        out = "F"
+    print(out.rjust(outspace, " "))
--- poly_pz/pzpoly.py
+++ poly_pz/pzpoly.py
@@ -1,40 +1,48 @@
-'''
+"""
 structure of polynomial: {exp: coef}
-'''
+"""
 from collections import defaultdict
+
 
 class polynomial:
     def __init__(self):
         self.eqn = defaultdict(lambda: 0)
-        
+
     def set_coef(self, coef, power):
-        if coef==0: return
+        if coef == 0:
+            return
         self.eqn[power] = coef
-    
+
     def __str__(self):
-        #tested to be 2x faster for n=100 than loop
-        return " + ".join([str(coef)+"x^"+ str(exp) for exp,coef in sorted(self.eqn.items())])
-    
+        # tested to be 2x faster for n=100 than loop
+        return " + ".join(
+            [
+                str(coef) + "x^" + str(exp)
+                for exp, coef in sorted(self.eqn.items())
+            ]
+        )
+
     def __add__(self, other):
         result = polynomial()
         result.eqn = self.eqn
         for exp, coef in other.eqn.items():
-            result.eqn[exp]+=coef
+            result.eqn[exp] += coef
         return result
-    
+
     def __mul__(self, other):
         result = polynomial()
         for e1, co1 in self.eqn.items():
             for e2, co2 in other.eqn.items():
-                result.eqn[e1+e2] += co1*co2
+                result.eqn[e1 + e2] += co1 * co2
         return result
-    
-    def __lshift__(self,other):
-        if(other[0]):
+
+    def __lshift__(self, other):
+        if other[0]:
             self.eqn[other[1]] = other[0]
         return self
 
+
 p1 = polynomial()
-p1<<[1,1]<<[0,2]<<[-3,4]
-print("p1",p1)
-print("p1*p1", p1*p1)
+p1 << [1, 1] << [0, 2] << [-3, 4]
+print("p1", p1)
+print("p1*p1", p1 * p1)
